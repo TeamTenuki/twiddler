7ddbc7cdd721b6670c0213579a06debd0c927711
destroycomputers <destroycomputers@protonmail.com>
2019-09-08 04:39:05 +0300

    Rename db.MustOpen to MustInit, add documentation.

--- db/db.go
+++ db/db.go
@@ -12,6 +12,11 @@ import (
 
 var db *sqlx.DB
 
+// Init initialises the DB connection, so that is now possible
+// to put it into context using NewContext function.
+// The DB file will be placed at given path, or, if path is an empty string,
+// the default path will be chosen with the name "twiddler.db" placed at
+// default config directory (see config.Dir).
 func Init(path string) (err error) {
 	dbFilepath := path
 
@@ -29,7 +34,8 @@ func Init(path string) (err error) {
 	return err
 }
 
-func MustOpen(path string) {
+// MustInit calls Init and panics on errors.
+func MustInit(path string) {
 	if err := Init(path); err != nil {
 		panic(err)
 	}
@@ -41,10 +47,14 @@ const (
 	dbContextKey contextKey = iota
 )
 
+// NewContext returns a context with DB connection in it.
+// The connection is retrievable with FromContext function.
 func NewContext(c context.Context) context.Context {
 	return context.WithValue(c, dbContextKey, db)
 }
 
+// FromContext retrieves a DB connection from context.
+// Function will crash if supplied context wasn't created by NewContext.
 func FromContext(c context.Context) *sqlx.DB {
 	return c.Value(dbContextKey).(*sqlx.DB)
 }
